3a8c5c19d40cfd22d20db1ca91a09c4fd3aec97d
steephan-s <steephanselvaraj@testpress.in>
2016-05-06 13:47:58 +0530

    Add SplashScreen Activity and a mainactivity
    Add Spalshscreen with 2 sec delay and a main activity

---
+++ app/src/main/java/testpress/testpress/com/todo/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package testpress.testpress.com.todo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import testpress.testpress.com.todo.R;
+
+public class SplashScreenActivity extends Activity {
+
+    // Splash screen timer
+    private static final int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start app main activity
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
